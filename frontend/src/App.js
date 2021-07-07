b09ca3839ce8c034e46817885b80f9ebd9e6afee
Max Smeets <71692651+MaxSmeets@users.noreply.github.com>
2021-07-07 14:59:51 +0200

    added routing to components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,55 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import AboutUs from "./components/about_us/about_us";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import Projects from "./components/projects/projects";
+import Quote from "./components/quote/quote";
+import Home from "./components/home/home";
 import logo from "./images/zephyr_transparent.png";
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <img src={logo} alt="logo" />
-      <h1>Coming soon...</h1>
-    </div>
-  );
-};
+    <Router>
+      <Header />
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                <img src={logo} alt="logo" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/quote">Get a Quote</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default App;
+        <Switch>
+          <Route path="/about">
+            <AboutUs />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/quote">
+            <Quote />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </Router>
+  );
+}
