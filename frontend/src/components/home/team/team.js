0b1ed4d44237b44f3cf8ad209691a5de2afb9887
Max Smeets <71692651+MaxSmeets@users.noreply.github.com>
2021-07-20 19:09:30 +0200

    added staff on homepage

--- frontend/src/components/home/team/team.js
+++ frontend/src/components/home/team/team.js
@@ -1,10 +1,54 @@
+import ReactRoundedImage from "react-rounded-image";
+
 import "./team.css";
+import baseAvatar from "../../../images/avatarPlaceholder.jpg";
 
 const Team = () => {
   return (
     <div className="team-bg">
-      <h1>Our Team</h1>
-      <h4>Take a look at our team</h4>
+      <h1 className="teamUpper">Our Team</h1>
+      <h4 className="teamSub">Take a look at our team</h4>
+      <div className="staffTables">
+        <table className="staff">
+          <tr>
+            <ReactRoundedImage
+              image={baseAvatar}
+              borderRadius="100"
+              roundedSize="0"
+            />
+          </tr>
+          <tr>
+            <h5 className="staffUpper">Connor van Wissen</h5>
+            <p className="staffSub">Web designer</p>
+          </tr>
+        </table>
+        <table className="staff">
+          <tr>
+            <ReactRoundedImage
+              image={baseAvatar}
+              borderRadius="100"
+              roundedSize="0"
+            />
+          </tr>
+          <tr>
+            <h5 className="staffUpper">Dave Wolfs</h5>
+            <p className="staffSub">Full Stack Developer</p>
+          </tr>
+        </table>
+        <table className="staff">
+          <tr>
+            <ReactRoundedImage
+              image={baseAvatar}
+              borderRadius="100"
+              roundedSize="0"
+            />
+          </tr>
+          <tr>
+            <h5 className="staffUpper">Max Smeets</h5>
+            <p className="staffSub">Full Stack Developer</p>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 };
